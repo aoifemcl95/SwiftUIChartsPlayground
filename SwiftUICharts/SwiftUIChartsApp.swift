684296ceaf68eebecf0cae0e9926470783a66659
Aoife McLaughlin <aoifemcl@gmail.com>
2023-05-25 22:21:48 +0100

    Initial Commit

---
+++ SwiftUICharts/SwiftUIChartsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIChartsApp.swift
+//  SwiftUICharts
+//
+//  Created by Aoife McLaughlin on 25/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIChartsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
